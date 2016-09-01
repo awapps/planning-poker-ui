29fe694d70a2fb0aa048e4d1a87d2bb1f516ea69
Eberhardt, Andre <santos.eberhardt@gmail.com>
2016-09-01 12:41:45 -0300

    Improving linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
     "extends": "airbnb",
     "rules": {
+        "comma-dangle": 0,
         "quotes": ["error", "single"],
+        "indent": ["error", 4],
         "react/jsx-indent": ["error", 4],
+        "react/jsx-indent-props": ["error", 4],
+        "react/jsx-closing-bracket-location": ["error", "after-props"],
         "react/jsx-filename-extension": 0
     },
     "plugins": [
